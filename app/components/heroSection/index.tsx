b1d510a60dba2ad38beeaeab435d2aa00c940d40
Parham Heydari <heidari1377@gmail.com>
2023-12-22 17:47:35 +0330

    🍱 add assets for the hero section

--- app/components/heroSection/index.tsx
+++ app/components/heroSection/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import Image from 'next/image';
+
+import smallLogo from '@/app/resources/images/logo.svg';
+import bigLogo from '@/app/resources/images/logo-icc.svg';
+import movingArticles from '@/app/resources/images/moving-articles.gif';
+
+import styles from './styles.module.scss';
 
 const HeroSection = () => (
-    <section>
-        HeroSection
+    <section className={styles.heroSection}>
+        <div className={styles.logoContainer} >
+            <div>
+                <Image alt='ICC' width={323} height={165} src={bigLogo} />
+                <Image alt='Jus Mundi' width={107} height={38} src={smallLogo} />
+                <Image alt='articles' width={479} height={497} src={movingArticles} />
+            </div>
+        </div>
+
+        <div className={styles.textContainer}>
+            <h1>
+                A Unique Partnership for the Publication of ICC Arbitration Awards
+            </h1>
+            <p className={styles.description}>
+                The International Chamber of Commerce (ICC) & Jus Mundi have joined forces to make ICC Arbitration Awards
+                and related materials freely available to the global legal community.
+            </p>
+            <button className={styles.button}>
+                Access ICC Awards
+            </button>
+        </div>
+
+        
     </section>
 );
 
