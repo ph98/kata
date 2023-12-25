7f2254c2e4ec093e1a1c10d97d9fdeef8f31e3d1
Parham Heydari <heidari1377@gmail.com>
2023-12-25 18:55:26 +0330

    ✨ add testimonialSection

--- app/components/testimonialSection/index.tsx
+++ app/components/testimonialSection/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import styles from './styles.module.scss';
+
 const TestimonialSection = () => (
-    <section>
-        TestimonialSection
+    <section className={styles.testimonialSection}>
+        <blockquote>
+            Publicising  and  disseminating  information  about  arbitration  has  been  one  of  ICC’s 
+            commitments  since  its  creation  and  an  instrumental  factor  in  facilitating  the  development  of trade worldwide.
+        </blockquote>
+        <p>
+            Note to the Parties and the Arbitral Tribunals {'\n'}
+            on the Conduct of Arbitration under the ICC Rules of Arbitration
+        </p>
+        <a href='#'>
+            read the whole document
+        </a>
     </section>
 );
 
