f1779339bc6bd9369edf32fb82bc2629c60f6476
Parham Heydari <heidari1377@gmail.com>
2023-12-25 17:04:49 +0330

    🍱 add bootstrap reset

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import type { Metadata } from 'next'
 
-
+import './resources/styles/global.scss';
 
 export const metadata: Metadata = {
   title: 'Kata',
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import HeroSection from './components/heroSection'
-import TestimonialSection from './components/testimonialSection'
-import PartnershipSection from './components/partnershipSection'
+
 import FeaturesSection from './components/featuresSection'
-import NewsFeedSection from './components/newsFeedSection'
+import HeroSection from './components/heroSection'
 import LogoSection from './components/logosSection'
+import NewsFeedSection from './components/newsFeedSection'
+import PartnershipSection from './components/partnershipSection'
+import TestimonialSection from './components/testimonialSection'
+
+import styles from './page.module.css'
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
-      <HeroSection />
-      <TestimonialSection />
-      <PartnershipSection />
-      <FeaturesSection />
-      <NewsFeedSection />
-      <LogoSection />
-    </main>
+      <main className={styles.main}>
+          <HeroSection />
+          <TestimonialSection />
+          <PartnershipSection />
+          <FeaturesSection />
+          <NewsFeedSection />
+          <LogoSection />
+      </main>
   )
+
 }
