5c6db905439b2d134ab0220217ba0a68f46d7986
Parham Heydari <heidari1377@gmail.com>
2023-12-26 16:42:24 +0330

    🐛 fix font

---
+++ app/resources/styles/fonts.ts
@@ -0,0 +1,27 @@
+// eslint-disable-next-line camelcase
+import { IBM_Plex_Sans, Lato } from "next/font/google"
+
+import './fonts.scss';
+
+const ibmPlexSans = IBM_Plex_Sans({
+    weight: [
+      '400',
+      '500',
+      '700'
+    ],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-ibm-plex'
+  })
+  
+  const lato = Lato({
+    weight: [
+      '400'
+    ],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-lato'
+  })
+
+export {ibmPlexSans, lato}
